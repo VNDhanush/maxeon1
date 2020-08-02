91b6a4ebd56d1c79c4af97e3ad69f05f0c5d0882
Dhanush <vndhanush51@gmail.com>
2020-08-02 23:10:08 +0530

    returning the commit

--- components/Home/Banner.js
+++ components/Home/Banner.js
@@ -1,51 +1,48 @@
 import React from 'react';
 import Link from 'next/link';
-import ban from '/images/banner.png';
 const isServer = typeof window === 'undefined';
 const WOW = !isServer ? require('wowjs') : null;
-const mainbanner = () => {
-  const imageUrl = window.innerWidth >= 650 ? desktopImage : ban;
 
-  class Banner extends React.Component {
-    componentDidMount() {
-      new WOW.WOW({
-        boxClass: 'wow',
-        animateClass: 'animated',
-        offset: 20,
-        mobile: true,
-        live: true
-      }).init();
-    }
+class Banner extends React.Component {
+  componentDidMount() {
+    new WOW.WOW({
+      boxClass: 'wow',
+      animateClass: 'animated',
+      offset: 20,
+      mobile: true,
+      live: true
+    }).init();
+  }
 
-    render() {
-      return (
-        <React.Fragment>
-          <div className="main-banner marketing-home" style={{ background: 'url(${imageUrl})' }}>
-            <div className="d-table">
-              <div className="d-table-cell">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-lg-6 col-md-12">
-                      <div className="hero-content">
-                        <h1>Where</h1>
-                        <h1>words</h1>
-                        <h1>fail,</h1>
-                        <h1>Music</h1>
-                        <h1>speaks.</h1>
+  render() {
+    return (
+      <React.Fragment>
+        <div className="main-banner marketing-home">
+          <div className="d-table">
+            <div className="d-table-cell">
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-6 col-md-12">
+                    <div className="hero-content">
+                      <h1>Where</h1>
+                      <h1>words</h1>
+                      <h1>fail,</h1> 
+                      <h1>Music</h1>
+                      <h1>speaks.</h1>                     
 
-
-                      </div>
+                      
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-
-
           </div>
-        </React.Fragment>
-      );
-    }
+
+         
+        </div>
+      </React.Fragment>
+    );
   }
 }
-  export default Banner;
+
+export default Banner;
